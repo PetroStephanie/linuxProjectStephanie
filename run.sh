bc0e8a506983903ce8679cfa91ce307c3e84642a
Stephanie Petrosyants <stephanie.petrosyants@example.com>
2018-11-14 14:27:56 -0700

    first commit

---
+++ run.sh
@@ -0,0 +1,31 @@
+{
+echo "<html>
+	
+	<head>
+		<title>CMPT251</title>
+	</head>
+
+	<body>
+		<h1>Welcome!</h1>
+
+	<a href= "A.html">Link to A</a>
+			
+	</body>
+</html>"
+
+}>>index.html
+
+{ 
+echo "<html>
+
+	<head>
+		<title>CMPT251A</title>
+	</head>
+
+	<body>
+		<h1>Welcome to A!</h1>
+	
+	</body>
+</html>"
+
+}>>A.html
